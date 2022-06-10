5b9370b7126e55faad8187f271780bee12b38245
Madej Zofia <Zofia.Madej@external.t-mobile.pl>
2022-06-10 14:22:43 +0200

    1.6 task api get data without keys names

--- src/App.vue
+++ src/App.vue
@@ -51,9 +51,13 @@ export default {
     fetch(
       'https://openlibrary.org/api/books?bibkeys=ISBN:9789123963140,ISBN:9780030234491,ISBN:9780320037825&format=json&jscmd=data')
       .then(response => response.json())
+      // .then(data => {
+      //   this.books.push({ title: data['ISBN:9789123963140'].title, price: 20 }, { title: data['ISBN:9780030234491'].title, price: 30 },
+      //     { title: data['ISBN:9780320037825'].title, price: 40 })
+      // })
       .then(data => {
-        this.books.push({ title: data['ISBN:9789123963140'].title, price: 20 }, { title: data['ISBN:9780030234491'].title, price: 30 },
-          { title: data['ISBN:9780320037825'].title, price: 40 })
+        this.books.push({ title: data[Object.keys(data)[0]].title, price: 20 }, { title: data[Object.keys(data)[1]].title, price: 30 },
+          { title: data[Object.keys(data)[2]].title, price: 40 })
       })
   }
 }
